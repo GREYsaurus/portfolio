544d892bae20ff7125249ca638306377df5ac93d
GREYsaurus <greysaurus@gmail.com>
2025-06-11 01:39:21 +0300

    please work

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react()],
-  base: '/portfolio/' // <- Replace with your repo name if different
 })
